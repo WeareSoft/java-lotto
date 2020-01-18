96a4705a9d0ed1678574d0ccf6c6d9c92eae70ec
doooyeon <ehdus5958@naver.com>
2020-01-19 03:12:25 +0900

    feat: Rank countOfBonusMatch

--- src/main/java/Rank.java
+++ src/main/java/Rank.java
@@ -1,20 +1,23 @@
 public enum Rank {
-	FIRST(6,  2000000000),
-	SECOND(5,  1500000),
-	THIRD(4, 50000),
-	FOURTH(3, 5000);
+	FIRST(6, 0, 2000000000),
+	SECOND(5, 1,  30000000),
+	THIRD(5, 0, 1500000),
+	FOURTH(4, 0, 50000),
+	FIFTH(3, 0, 5000);
 
 	private int countOfMatch;
+	private int countOfBonusMatch;
 	private int prizeMoney;
 
-	Rank(final int countOfMatch, final int prizeMoney) {
+	Rank(final int countOfMatch, final int countOfBonusMatch, final int prizeMoney) {
 		this.countOfMatch = countOfMatch;
+		this.countOfBonusMatch = countOfBonusMatch;
 		this.prizeMoney = prizeMoney;
 	}
 
-	public static Rank valueOf(final int countOfMatch) {
+	public static Rank valueOf(final int countOfMatch, final int countOfBonusMatch) {
 		for (final Rank rank : Rank.values()) {
-			if (rank.countOfMatch == countOfMatch) {
+			if (rank.countOfMatch == countOfMatch && rank.countOfBonusMatch == countOfBonusMatch) {
 				return rank;
 			}
 		}
