ce9d804c2ccc7fdecb1acd4af65071434ab4d628
Delf-Lee <ttjs2008@naver.com>
2020-01-19 07:32:48 +0900

    LottoStatistics의 필요없는 책임 삭제 및 캡슐화
    - iterator 구현, 출력 책임 삭제

--- src/main/java/domain/LottoStatistics.java
+++ src/main/java/domain/LottoStatistics.java
@@ -1,43 +1,35 @@
-package output;
+package domain;
 
-import domain.lotto.Rank;
 import domain.lotto.Lotto;
 import domain.lotto.LottoCollection;
 import domain.money.Money;
 
+import java.util.Iterator;
 import java.util.Map;
 
 /**
  * @author delf
  */
-public class Statistics {
+public class LottoStatistics implements Iterable<Rank> {
 	private Map<Rank, Integer> counter;
-	private float rateOfReturn;
+	private Money invest;
 
-	public Statistics(Lotto winningLotto, LottoCollection lottoCollection, Money money) {
+	public LottoStatistics(Lotto winningLotto, LottoCollection lottoCollection, Money money) {
 		this.counter = getCounter(winningLotto, lottoCollection);
-		this.rateOfReturn = calcRor() / money.getAmount();
+		this.invest = money;
 	}
 
-	private float calcRor() {
+	public float getRor() {
 		int sum = 0;
 		for (Rank rank : counter.keySet()) {
 			sum += rank.getProfit(counter.get(rank));
 		}
-		return sum / counter.size();
+
+		System.out.println(invest.getAmount());
+		return (float) sum / invest.getAmount();
 	}
 
 	private final static Rank[] PRINT_ORDER_RANK = {Rank.FIFTH, Rank.FOURTH, Rank.THIRD, /*Rank.SECOND, */Rank.FIRST};
-
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		for (Rank rank : PRINT_ORDER_RANK) {
-			sb.append(rank).append(" - ").append(counter.get(rank)).append("개\n");
-		}
-
-		return String.format("%s\n총 수익률은 %.1f%%입니다.", sb, rateOfReturn);
-	}
 
 	private Map<Rank, Integer> getCounter(Lotto winningLotto, LottoCollection lottoCollection) {
 		Map<Rank, Integer> counter = Rank.RANK_COUNTER();
@@ -45,5 +37,14 @@ public class Statistics {
 			counter.merge(winningLotto.match(lotto), 1, Integer::sum);
 		}
 		return counter;
+	}
+
+	@Override
+	public Iterator<Rank> iterator() {
+		return counter.keySet().iterator();
+	}
+
+	public int getCount(Rank rank) {
+		return counter.get(rank);
 	}
 }
