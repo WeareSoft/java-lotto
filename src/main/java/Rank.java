0897adcf2da1058342c464c940e6ede0653c38a3
doooyeon <ehdus5958@naver.com>
2020-01-19 03:02:21 +0900

    fix: 오타

--- src/main/java/Rank.java
+++ src/main/java/Rank.java
@@ -1,8 +1,8 @@
 public enum Rank {
 	FIRST(6,  2000000000),
-	THIRD(5,  1500000),
-	FOURTH(4, 50000),
-	FIFTH(3, 5000);
+	SECOND(5,  1500000),
+	THIRD(4, 50000),
+	FOURTH(3, 5000);
 
 	private int countOfMatch;
 	private int prizeMoney;
